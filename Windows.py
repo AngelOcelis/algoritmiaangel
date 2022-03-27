bab0502344c4b35bc8c5226c3160c5aa8399e318
AngelOcelis <101607546+AngelOcelis@users.noreply.github.com>
2022-03-26 19:39:43 -0500

    Add files via upload

---
+++ Windows.py
@@ -0,0 +1,14 @@
+tecl1= input("Digite la opcion de la tecl 1: ")
+tecl2= input("Digite la opcion de la tecl 2: ")
+if tecl1=="ctrl" and tecl2=="v":
+    print("Pegar")
+elif tecl1=="ctrl" and tecl2=="c": 
+    print("Copiar")
+elif tecl1=="ctrl" and tecl2=="a": 
+    print("Seleccionar Todo el Texto")
+elif tecl1=="ctrl" and tecl2=="x": 
+    print("Cortar")
+elif tecl1=="ctrl" and tecl2== "n":
+    print ("Nuevo Documento")
+else: 
+    print("NO FUNCIONA")
